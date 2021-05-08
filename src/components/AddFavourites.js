9919ccce79b681ff479145de513d8b4979a854e6
shubh <thakres955@gmail.com>
2021-05-08 14:28:06 +0530

    fix type error

--- src/components/AddFavourites.js
+++ src/components/AddFavourites.js
@@ -5,15 +5,15 @@ const AddFavourite = () => {
 		<>
 			<span className='mr-2'>Add to Favourites</span>
 			<svg
-				width='2em'
+				width='1em'
 				height='1em'
 				viewBox='0 0 16 16'
-				className='bi bi-heart-fill'
+				class='bi bi-heart-fill'
 				fill='red'
 				xmlns='http://www.w3.org/2000/svg'
 			>
 				<path
-					fillRule='evenodd'
+					fill-rule='evenodd'
 					d='M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z'
 				/>
 			</svg>
